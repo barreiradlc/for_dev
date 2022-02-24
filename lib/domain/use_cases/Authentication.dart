b773c472afea0ac298b7751db6babaca60ef1170
Augusto Barreira <barreira266@hotmail.com>
2022-02-24 07:35:40 -0300

    first commit

---
+++ lib/domain/use_cases/Authentication.dart
@@ -0,0 +1,7 @@
+import 'package:meta/meta.dart';
+
+import 'package:for_dev/domain/entities/account_entity.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth({@required String email, @required String password});
+}
