b3f0635f524ab388786f7fcc2708a0f42a4aa2f8
Augusto Barreira <barreira266@hotmail.com>
2022-02-25 22:40:24 -0300

    Should return null on 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -69,5 +69,12 @@ void main() {
 
       expect(response, null);
     });
+    test('Should return null if post return 204', () async {
+      mockResponse(204, body: '');
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
   });
 }
