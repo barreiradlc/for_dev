47f2df6c77773186e355a5354cfa56e6e1b95355
Augusto Barreira <barreira266@hotmail.com>
2022-02-25 23:41:09 -0300

    Should throw serverError if invalid Http method is provided

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -15,12 +15,18 @@ class HttpAdapter implements HttpClient {
     final headers = {'content-type': 'application/json', 'accept': 'application/json'};
     final jsonBody = body != null ? jsonEncode(body) : null;
 
-    final response = await client.post(url, headers: headers, body: jsonBody);
+    var response = Response('', 500);
+    if (method == 'post') {
+      response = await client.post(url, headers: headers, body: jsonBody);
+    }
 
     return _handleResponse(response);
   }
 
   Map _handleResponse(Response response) {
+    if (response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    }
     if (response.statusCode == 204) {
       return null;
     }
@@ -39,6 +45,6 @@ class HttpAdapter implements HttpClient {
     if (response.statusCode == 500) {
       throw HttpError.serverError;
     }
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    throw HttpError.serverError;
   }
 }
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -19,6 +19,13 @@ void main() {
     sut = HttpAdapter(client);
     url = faker.internet.httpUrl();
   });
+  group('shared', () {
+    test('Should throw serverError if invalid Http method is provided', () async {
+      final future = sut.request(url: url, method: 'invalid_method', body: {'any_key': 'any_value'});
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
   group('post', () {
     PostExpectation mockRequest() => when(client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
 
