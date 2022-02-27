49db663240e6a53febfd1c3fff653b103501705c
Augusto Barreira <barreira266@hotmail.com>
2022-02-27 10:28:28 -0300

    Should call validation with correct email

---
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -0,0 +1,30 @@
+import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class StreamLoginPresenter {
+  final Validation validation;
+
+  StreamLoginPresenter({ required this.validation });
+
+  void validateEmail(String email){
+    validation.validate(field: 'email', value: email);
+  }
+}
+
+abstract class Validation {
+  String? validate({ required String field, required String value });
+}
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  test('Should call validation with correct email', (){
+    final validation = ValidationSpy();
+    final sut = StreamLoginPresenter(validation: validation);
+    final email = faker.internet.email();
+    sut.validateEmail(email);
+    
+    verify(() => validation.validate(field: 'email', value: email)).called(1);
+  });
+}
