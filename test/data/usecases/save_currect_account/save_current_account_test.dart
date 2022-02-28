28ee53f27ff8755e7266c1eabc8075f46190a35f
Augusto Barreira <barreira266@hotmail.com>
2022-02-28 15:56:15 -0300

    Move setup to top

--- test/data/usecases/save_currect_account/save_current_account_test.dart
+++ test/data/usecases/save_currect_account/save_current_account_test.dart
@@ -1,9 +1,10 @@
 import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:mocktail/mocktail.dart';
+
 import 'package:for_dev/domain/entities/account_entity.dart';
 import 'package:for_dev/domain/helpers/domain_error.dart';
 import 'package:for_dev/domain/use_cases/save_currect_account.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:test/test.dart';
 
 class LocalSaveCurrentAccount implements SaveCurrectAccount {
   late SaveSecureCacheStorage saveSecureCacheStorage;
@@ -26,21 +27,23 @@ abstract class SaveSecureCacheStorage {
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {}
 
 main() {
+  late SaveSecureCacheStorageSpy saveSecureCacheStorage;
+  late LocalSaveCurrentAccount sut;
+  late AccountEntity account;
+
+  setUp(() {
+    saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    account = AccountEntity(faker.guid.guid());
+  });
   
   test('Should saveSecure cache storage with correct values', () async {
-    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
-    final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
-    final account = AccountEntity(faker.guid.guid());
-
     await sut.save(account);
 
     verify(() => saveSecureCacheStorage.saveSecure(key: 'token', value: account.token));
   });
   
-  test('Should throws Unexpected if cache storage throws', () async {
-    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
-    final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
-    final account = AccountEntity(faker.guid.guid());
+  test('Should throws Unexpected if cache storage throws', () async {    
     when(() => saveSecureCacheStorage.saveSecure(key: any(named: 'key'), value: any(named: 'value')))
       .thenThrow(Exception());
 
