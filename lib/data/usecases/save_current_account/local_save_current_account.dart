3fe3c1ad8c6a993cab8089315452251f8c63f5e3
Augusto Barreira <barreira266@hotmail.com>
2022-02-28 16:05:08 -0300

    move method to production folders

---
+++ lib/data/usecases/save_current_account/local_save_current_account.dart
@@ -0,0 +1,18 @@
+import 'package:for_dev/data/cache/save_secure_cache_storage.dart';
+import 'package:for_dev/domain/entities/account_entity.dart';
+import 'package:for_dev/domain/helpers/domain_error.dart';
+import 'package:for_dev/domain/use_cases/save_currect_account.dart';
+
+class LocalSaveCurrentAccount implements SaveCurrectAccount {
+  late SaveSecureCacheStorage saveSecureCacheStorage;
+
+  LocalSaveCurrentAccount({ required this.saveSecureCacheStorage });
+
+  Future<void> save(AccountEntity account) async {
+    try {
+      await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+    } catch (e) {
+      throw DomainError.unexpected;
+    }
+  }
+}
--- test/data/usecases/save_currect_account/save_current_account_test.dart
+++ test/data/usecases/save_currect_account/save_current_account_test.dart
@@ -2,27 +2,10 @@ import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 import 'package:mocktail/mocktail.dart';
 
+import 'package:for_dev/data/usecases/save_current_account/local_save_current_account.dart';
+import 'package:for_dev/data/cache/save_secure_cache_storage.dart';
 import 'package:for_dev/domain/entities/account_entity.dart';
 import 'package:for_dev/domain/helpers/domain_error.dart';
-import 'package:for_dev/domain/use_cases/save_currect_account.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrectAccount {
-  late SaveSecureCacheStorage saveSecureCacheStorage;
-
-  LocalSaveCurrentAccount({ required this.saveSecureCacheStorage });
-
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
-    } catch (e) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCacheStorage {
-  Future<void>? saveSecure({ required String key, required String value});
-}
 
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {}
 
